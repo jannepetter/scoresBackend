60760567ff6f806ed7bbaa107d2777d1a2e0062d
jannepetter <peterdev@gmail.com>
2021-05-22 21:54:06 +0300

    changed ports

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('./app')
 const config = require('./utils/config')
 
-const PORT = config.PORT
+const PORT = config.PORT || 5000
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
